91d096253692f06b6c7997dd96288349eaa22980
root <paltuss92@gmail.com>
2023-08-14 21:31:42 +0300

    last

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default_stub_key')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
